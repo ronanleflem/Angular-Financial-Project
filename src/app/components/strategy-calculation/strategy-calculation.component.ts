eaab302c3dafe5bfa0f062510382549d482a757d
ronanleflem <54776258+ronanleflem@users.noreply.github.com>
2025-07-05 20:56:34 +0200

    feat: load strategies from backend

--- src/app/components/strategy-calculation/strategy-calculation.component.ts
+++ src/app/components/strategy-calculation/strategy-calculation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {JsonPipe, NgForOf, NgIf} from '@angular/common';
@@ -16,8 +16,8 @@ import {TradingDataService} from '../../services/trading-data.service';
   standalone: true,
   styleUrls: ['./strategy-calculation.component.css']
 })
-export class StrategyCalculationComponent {
-  strategies: string[] = ['Stratégie 1', 'Stratégie 2', 'Stratégie 3', 'Trend Following']; // Tu peux les charger dynamiquement si besoin
+export class StrategyCalculationComponent implements OnInit {
+  strategies: string[] = [];
   symbols: string[] = ['ES', 'NQ', 'YM', 'EURUSD', 'BTCUSDT']; // Exemples de symboles
   comparedSymbol: string[] = ['ES', 'NQ', 'YM', 'EURUSD', 'BTCUSDT']; // Exemples de symboles
   timeframes: string[] = ['1min', '5min', '15min', '1h'];
@@ -32,12 +32,32 @@ export class StrategyCalculationComponent {
 
   constructor(private http: HttpClient, private tradingService: TradingDataService) {}
 
+  ngOnInit(): void {
+    this.tradingService.listStrategies().subscribe({
+      next: (names) => {
+        this.strategies = names;
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement des stratégies :', err);
+      }
+    });
+  }
+
   calculateStrategy() {
     if (!this.selectedStrategy || !this.selectedSymbol || !this.selectedTimeframe || !this.startDate || !this.endDate || !this.selectedComparedSymbol) {
       alert('Tous les champs sont requis pour lancer le calcul !');
       return;
     }
-    this.result = this.tradingService.getCalculationStrategy(this.selectedStrategy,this.selectedSymbol, this.selectedComparedSymbol, this.selectedTimeframe,this.startDate,this.endDate).subscribe({
+    this.result = this.tradingService
+      .getCalculationStrategy(
+        this.selectedStrategy,
+        this.selectedSymbol,
+        this.selectedComparedSymbol,
+        this.selectedTimeframe,
+        this.startDate,
+        this.endDate
+      )
+      .subscribe({
       next: (response) => {
         console.log('Résultat de la stratégie', response);
         this.result = response;
