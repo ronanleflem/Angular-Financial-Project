f4bc72420c6cb92974bf3feb9d8cda13cfcaccef
ronanleflem <ronanleflem@gmail.com>
2025-07-06 15:42:52 +0200

    Configure environment path

--- src/app/services/trading-data.service.ts
+++ src/app/services/trading-data.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TradingDataService {
-  private apiUrl = 'http://localhost:8090'; // Adapte l'URL selon ton API
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
